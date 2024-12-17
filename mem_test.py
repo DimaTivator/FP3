2b5b9a064ac9d522ae7c3b559828b34ee3604b74
DimaTivator <andriianov164@gmail.com>
2024-12-17 16:35:49 +0300

    add: memory test on 100mb

--- mem_test.py
+++ mem_test.py
@@ -1,28 +1,49 @@
 import subprocess
 import resource
+from tqdm import tqdm
+from pathlib import Path
 
-def run_ocaml_program(input_data, method, step, window_size):
-    command = ['dune', 'exec', 'FP3', '--', '-m'] + [method] + ['-s', str(step), '-w', str(window_size)]
+
+def avg_memory_usage(
+    input_data: str, method: str, step: float, window_size: int
+) -> float:
+    sum_memory = 0
+    for _ in tqdm(range(10)):
+        command = (
+            ["dune", "exec", "FP3", "--", "-m"]
+            + [method]
+            + ["-s", str(step), "-w", str(window_size)]
+        )
+        resource.setrlimit(
+            resource.RLIMIT_AS, (resource.RLIM_INFINITY, resource.RLIM_INFINITY)
+        )
+        process = subprocess.Popen(
+            command,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        _, stderr = process.communicate(input=input_data.encode())
+
+        usage = resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss
+        sum_memory += usage / 1024
+
+        if stderr:
+            print("Error:", stderr.decode())
+
+    return sum_memory / 10
+
+
+with open("mem_test_input", "r") as file:
+    data_100mb = file.read()
     
-    resource.setrlimit(resource.RLIMIT_AS, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
+with open("small_test_input", "r") as file:
+    data_1mb = file.read()
 
-    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print("Running")
-    _, stderr = process.communicate(input=input_data.encode())
+method = "lagrange"
+step = 1
+window_size = 4
 
-    usage = resource.getrusage(resource.RUSAGE_CHILDREN)
-    max_memory = usage.ru_maxrss / 1024
-    print(f"Maximum memory usage: {max_memory:.2f} MB")
-
-    if stderr:
-        print("Error:", stderr.decode())
-
-if __name__ == "__main__":
-    with open("mem_test_input", 'r') as file:
-        input_data = file.read()
-    
-    method = 'lagrange'
-    step = 1
-    window_size = 4
-    
-    run_ocaml_program(input_data, method, step, window_size)
+avg_memory_usage_100mb = avg_memory_usage(data_100mb, method, step, window_size)
+avg_memory_usage_1mb = avg_memory_usage(data_1mb, method, step, window_size)
+print(avg_memory_usage_100mb - avg_memory_usage_1mb)
